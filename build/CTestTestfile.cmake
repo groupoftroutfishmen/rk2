7e3b97229f92b0659e08b143f6d4e23b182f7f4d
groupoftroutfishmen <gchibanov@yandex.ru>
2024-06-06 09:38:52 +0300

    r

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(testS "/home/georgy/rk2/23Design_Patterns_CPP_Codeblocks/ChainOfResponsibility/build/RK_TEST")
-set_tests_properties(testS PROPERTIES  _BACKTRACE_TRIPLES "/home/georgy/rk2/23Design_Patterns_CPP_Codeblocks/ChainOfResponsibility/CMakeLists.txt;41;add_test;/home/georgy/rk2/23Design_Patterns_CPP_Codeblocks/ChainOfResponsibility/CMakeLists.txt;0;")
+set_tests_properties(testS PROPERTIES  _BACKTRACE_TRIPLES "/home/georgy/rk2/23Design_Patterns_CPP_Codeblocks/ChainOfResponsibility/CMakeLists.txt;40;add_test;/home/georgy/rk2/23Design_Patterns_CPP_Codeblocks/ChainOfResponsibility/CMakeLists.txt;0;")
 subdirs("_deps/googletest-build")
